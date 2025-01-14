1c9b10c212de4aad5cec5ed4c2609540e235fb5d
Rok Cigut <roki.cigut@gmail.com>
2025-01-14 18:17:34 +0100

    returned appuser and cleared dbinitializer as its already created.

--- Data/LibraryContext.cs
+++ Data/LibraryContext.cs
@@ -15,14 +15,4 @@ public class LibraryContext : IdentityDbContext<ApplicationUser>
     public DbSet<Author> Authors { get; set; }
     public DbSet<Subsidiary> Subsidiaries { get; set; }
     public DbSet<Loan> Loans { get; set; }
-
-    protected override void OnModelCreating(ModelBuilder modelBuilder)
-    {
-        base.OnModelCreating(modelBuilder);
-        modelBuilder.Entity<Author>().ToTable("Author");
-        modelBuilder.Entity<Material>().ToTable("Material");
-        modelBuilder.Entity<Member>().ToTable("Member");
-        modelBuilder.Entity<Subsidiary>().ToTable("Subsidiary");
-        modelBuilder.Entity<Loan>().ToTable("Loan");
-    }
 }
